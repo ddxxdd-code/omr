a40e2f8beaedb3e4c171cc4a4d50d9741c3f1130
Dedong Xie <dedong.xie@mail.utoronto.ca>
2023-09-17 02:03:06 -0400

    Commit test
    Signed-off-by: Dedong Xie <dedong.xie@mail.utoronto.ca>

--- compiler/env/RegionLog.cpp
+++ compiler/env/RegionLog.cpp
@@ -1,4 +1,23 @@
-// TODO: Add header
+/*******************************************************************************
+ * Copyright (c) 2023, 2023 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
 #include "env/CompilerEnv.hpp"
 #include "env/RegionLog.hpp"
 #include <execinfo.h>
@@ -15,14 +34,24 @@ RegionLog::RegionLog(int startTime, size_t startBytesAllocated, bool isStack) :
    _endBytesAllocated(0),
    _prev(NULL),
    _next(NULL),
-   _allocMap(PersistentUnorderedMap<AllocEntry, size_t>::allocator_type(TR::Compiler->persistentAllocator()))
+   _allocMap(decltype(_allocMap)::allocator_type(TR::Compiler->persistentAllocator()))
       {
       _startTime = startTime;
       _startBytesAllocated = startBytesAllocated;
       _isStack = isStack;
-      void *trace[REGION_BACKTRACE_DEPTH + 2];
-      unw_backtrace(trace, REGION_BACKTRACE_DEPTH + 2);
-      memcpy(_regionTrace, &trace[2], REGION_BACKTRACE_DEPTH * sizeof(void *));
+      if (isStack)
+         {
+         // Stack region has a wrapper outside of region's initializer, so need an extra level of backtrace to get the region's instanciation call
+         void *trace[REGION_BACKTRACE_DEPTH + 3];
+         unw_backtrace(trace, REGION_BACKTRACE_DEPTH + 3);
+         memcpy(_regionTrace, &trace[2], REGION_BACKTRACE_DEPTH * sizeof(void *));
+         }
+      else
+         {
+         void *trace[REGION_BACKTRACE_DEPTH + 2];
+         unw_backtrace(trace, REGION_BACKTRACE_DEPTH + 2);
+         memcpy(_regionTrace, &trace[1], REGION_BACKTRACE_DEPTH * sizeof(void *));
+         }
       }
 
 void
@@ -153,13 +182,13 @@ RegionLog::accumulateMemoryRelease(size_t freedBytesAllocated, size_t freedBytes
    _bytesSegmentProviderRealInUseFreed += freedRealBytesInUse;
    }
 
-// size_t
-// AllocEntry::Hash::operator()(const AllocEntry& k) const noexcept
-//    {
-//    size_t result = 0;
-//    for (int i = 0; i < MAX_BACKTRACE_SIZE; i++) 
-//       {
-//       result ^= std::hash<void *>()(k._trace[i]);
-//       }
-//    return result;
-//    }
+size_t
+AllocEntry::Hash::operator()(const AllocEntry& k) const noexcept
+   {
+   size_t result = 0;
+   for (int i = 0; i < MAX_BACKTRACE_SIZE; i++) 
+      {
+      result ^= std::hash<void *>()(k._trace[i]);
+      }
+   return result;
+   }
--- compiler/env/RegionLog.hpp
+++ compiler/env/RegionLog.hpp
@@ -1,4 +1,23 @@
-// TODO: Add header
+/*******************************************************************************
+ * Copyright (c) 2023, 2023 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
 #ifndef OMR_REGION_LOG_HPP
 #define OMR_REGION_LOG_HPP
 
@@ -6,9 +25,9 @@
 #include "env/PersistentAllocator.hpp"
 #include <unordered_map>
 
-#define MAX_BACKTRACE_SIZE 10
-#define REGION_BACKTRACE_DEPTH 3
-#define TARGET_EXECUTABLE_FILE "libj9jit"
+#define MAX_BACKTRACE_SIZE 10 // Need 10 levels of backtracing to get the call site of a Region::allocate() call to fully recover the call site outside of wrapped lib functions
+#define REGION_BACKTRACE_DEPTH 1
+#define TARGET_EXECUTABLE_FILE "libj9jit" // Locate compiled .so file to reduce common prefix in translated backtraces.
 
 template<typename K, typename V>
 using PersistentUnorderedMapAllocator = TR::typed_allocator<std::pair<const K, V>, TR::PersistentAllocator &>;
@@ -18,28 +37,13 @@ using PersistentUnorderedMap = std::unordered_map<K, V, H, E, PersistentUnordere
 struct AllocEntry
    {
    void *_trace[MAX_BACKTRACE_SIZE];
-   // struct Hash { size_t operator()(const AllocEntry& k) const noexcept; };
+   struct Hash { size_t operator()(const AllocEntry& k) const noexcept; };
 
    bool operator==(const AllocEntry &other) const 
       {
       return memcmp(_trace, other._trace, sizeof(void *)*MAX_BACKTRACE_SIZE) == 0;
       }
    };
-namespace std {
-   template <>
-   struct hash<AllocEntry>
-      {
-      size_t operator()(const AllocEntry& k) const
-         {
-         size_t result = 0;
-         for (int i = 0; i < MAX_BACKTRACE_SIZE; i++) 
-            {
-            result ^= hash<void *>()(k._trace[i]);
-            }
-         return result;
-         }
-      };
-}
 
 class RegionLog
    {
@@ -57,22 +61,22 @@ public:
    void setEndBytesAllocated(size_t endBytesAllocated) { _endBytesAllocated = endBytesAllocated; };
    void accumulateMemoryIncrease(size_t deltaBytesAllocated, size_t deltaBytesInUse, size_t deltaRealInUse);
    void accumulateMemoryRelease(size_t freedBytesAllocated, size_t freedBytesInUse, size_t freedRealBytesInUse);
-   bool getIsStack() { return _isStack; };
-   int getStartTime() { return _startTime; };
-   int getEndTime() { return _endTime; };
-   size_t getBytesAllocated() { return _bytesAllocated; };
-   size_t getStartBytesAllocated() { return _startBytesAllocated; };
-   size_t getEndBytesAllocated() { return _endBytesAllocated; };
-   size_t getBytesSegmentProviderAllocated() { return _bytesSegmentProviderAllocated; };
-   size_t getBytesSegmentProviderFreed() { return _bytesSegmentProviderFreed; };
-   size_t getBytesSegmentProviderInUseAllocated() { return _bytesSegmentProviderInUseAllocated; };
-   size_t getBytesSegmentProviderInUseFreed() { return _bytesSegmentProviderInUseFreed; };
-   size_t getBytesSegmentProviderRealInUseAllocated() { return _bytesSegmentProviderRealInUseAllocated; };
-   size_t getBytesSegmentProviderRealInUseFreed() { return _bytesSegmentProviderRealInUseFreed; };
+   bool getIsStack() const { return _isStack; };
+   int getStartTime() const { return _startTime; };
+   int getEndTime() const { return _endTime; };
+   size_t getBytesAllocated() const { return _bytesAllocated; };
+   size_t getStartBytesAllocated() const { return _startBytesAllocated; };
+   size_t getEndBytesAllocated() const { return _endBytesAllocated; };
+   size_t getBytesSegmentProviderAllocated() const { return _bytesSegmentProviderAllocated; };
+   size_t getBytesSegmentProviderFreed() const { return _bytesSegmentProviderFreed; };
+   size_t getBytesSegmentProviderInUseAllocated() const { return _bytesSegmentProviderInUseAllocated; };
+   size_t getBytesSegmentProviderInUseFreed() const { return _bytesSegmentProviderInUseFreed; };
+   size_t getBytesSegmentProviderRealInUseAllocated() const { return _bytesSegmentProviderRealInUseAllocated; };
+   size_t getBytesSegmentProviderRealInUseFreed() const { return _bytesSegmentProviderRealInUseFreed; };
 
    void *_regionTrace[REGION_BACKTRACE_DEPTH];
    // Logical timestamp for start of the region
-   PersistentUnorderedMap<AllocEntry, size_t> _allocMap;
+   PersistentUnorderedMap<AllocEntry, size_t, AllocEntry::Hash> _allocMap;
 
    // pointers to prev ane next
    RegionLog *_prev = NULL;
